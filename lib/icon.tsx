53d297b6022da15d1d372a55b4652615bd169fd9
徐金俊 <939160741@qq.com>
2021-08-12 15:28:15 +0800

    feat(xjj): 函数参数结构赋值

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -7,13 +7,12 @@ interface IconProps extends React.SVGAttributes<SVGElement>{
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-  const {className, ...restProps} = props
+const Icon: React.FunctionComponent<IconProps> = ({className, name, ...restProps}) => {
   return (
     <svg className={classes('moore-ui-icon', className)}
          {...restProps}
     >
-      <use xlinkHref={`#${props.name}`}/>
+      <use xlinkHref={`#${name}`}/>
     </svg>
   )
 }
