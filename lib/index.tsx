58230a6c1f3e680902bbd5bc5729aaf0f735ae6d
徐金俊 <939160741@qq.com>
2021-08-16 15:42:49 +0800

    feat(xjj): circle ci 持续集成

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,1 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import Icon from './icon/icon'
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target)
-}
-
-ReactDOM.render(<div>
-  <Icon name="wechat"
-        onClick={fn}
-        onMouseLeave={fn}
-        onMouseEnter={fn}
-  />
-</div>, document.querySelector('#root'))
+export {default as Icon} from './icon/icon';
