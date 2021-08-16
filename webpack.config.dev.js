0c0286f31639d482b14ca30bc8e13c5d8a424429
徐金俊 <939160741@qq.com>
2021-08-16 16:30:38 +0800

    feat(xjj): 添加测试数据 update circle ci

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,13 @@ const base = require('./webpack.config.js')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = Object.assign({}, base, {
   mode: 'development',
+  entry: {
+    example: './example.tsx',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Moore",
-      template: 'index.html',
+      template: 'example.html',
     })
   ],
 })
