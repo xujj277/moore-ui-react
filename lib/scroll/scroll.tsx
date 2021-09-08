24e2c430c4e8b741869e500cb9dc0f31b758db4e
徐金俊 <939160741@qq.com>
2021-09-08 11:25:15 +0800

    feat(xjj): 初版scroll

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import scrollbarWidth from './scrollbar-width'
+import './scroll.scss'
 
-interface Props extends React.HTMLAttributes<HTMLElement>{}
+interface Props extends React.HTMLAttributes<HTMLElement> {}
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const {children, ...restProps} = props
+  const [barHeight, setBarHeight] = useState(0)
+  const [barTop, setBarTop] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const viewHeight = containerRef.current!.getBoundingClientRect().height
+    const scrollHeight = containerRef.current!.scrollHeight
+    setBarHeight(viewHeight * viewHeight / scrollHeight)
+  }, [])
+  const onScroll = () => {
+    const {current} = containerRef
+    const viewHeight = current!.getBoundingClientRect().height
+    const scrollHeight = current!.scrollHeight
+    const scrollTop = current!.scrollTop
+    setBarTop(scrollTop * viewHeight / scrollHeight)
+  }
   return (
     <div className={'moore-scroll'} {...restProps}>
-      <div className={'moore-scroll-inner'}  style={{right: -scrollbarWidth()}}>
+      <div className={'moore-scroll-inner'} style={{right: -scrollbarWidth()}}
+           ref={containerRef}
+           onScroll={onScroll}
+      >
         {children}
+      </div>
+      <div className={'moore-scroll-track'}>
+        <div className={'moore-scroll-bar'}
+             style={{height: barHeight, transform: `translateY(${barTop}px)`}}
+        />
       </div>
     </div>
   )
