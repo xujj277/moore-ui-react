88eca8b7eab5d6d3413a5a009e07845fda81150e
徐金俊 <939160741@qq.com>
2021-09-09 11:36:46 +0800

    feat(xjj): 滚动条滚动移动内容

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { MouseEventHandler, useEffect, useRef, useState } from 'react'
 import scrollbarWidth from './scrollbar-width'
 import './scroll.scss'
 
@@ -21,6 +21,42 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
     const scrollTop = current!.scrollTop
     setBarTop(scrollTop * viewHeight / scrollHeight)
   }
+  const draggingRef = useRef(false)
+  const firstYRef = useRef(0)
+  const onMouseDownBar: MouseEventHandler = (e) => {
+    draggingRef.current = true
+    firstYRef.current = e.clientY
+    console.log(e)
+  }
+  const onMouseUpBar = () => {
+    draggingRef.current = false
+    console.log(2)
+  }
+  const onMouseMoveBar = (e: MouseEvent) => {
+    if (draggingRef.current) {
+      const delta = e.clientY - firstYRef.current
+      const newBarTop = barTop + delta
+      setBarTop(newBarTop)
+      const viewHeight = containerRef.current!.getBoundingClientRect().height
+      const scrollHeight = containerRef.current!.scrollHeight
+      containerRef.current!.scrollTop = scrollHeight * newBarTop / viewHeight
+      console.log(3)
+    }
+  }
+  const onSelect = (e: Event) => {
+    if (draggingRef.current) { e.preventDefault() }
+  }
+  useEffect(() => {
+    document.addEventListener('mouseup', onMouseUpBar)
+    document.addEventListener('mousemove', onMouseMoveBar)
+    document.addEventListener('selectstart', onSelect)
+
+    return () => {
+      document.removeEventListener('mouseup', onMouseUpBar)
+      document.removeEventListener('mousemove', onMouseMoveBar)
+      document.removeEventListener('selectstart', onSelect)
+    }
+  }, [])
   return (
     <div className={'moore-scroll'} {...restProps}>
       <div className={'moore-scroll-inner'} style={{right: -scrollbarWidth()}}
@@ -32,6 +68,7 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
       <div className={'moore-scroll-track'}>
         <div className={'moore-scroll-bar'}
              style={{height: barHeight, transform: `translateY(${barTop}px)`}}
+             onMouseDown={onMouseDownBar}
         />
       </div>
     </div>
