c0e1775e23e52e392f75ce52eecbb14cd04dac80
徐金俊 <939160741@qq.com>
2021-09-27 15:52:48 +0800

    feat(xjj): update

--- lib/citySelect/citySelect.tsx
+++ lib/citySelect/citySelect.tsx
@@ -1,19 +1,20 @@
-import React, { Dispatch, SetStateAction, useState } from 'react'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
 import './citySelect.scss'
 
 interface Props {
   dataSource: string[];
-  onChange: (p1: string) => void;
+  onChange: (value: string) => void;
 }
 
 interface Context {
   map: Object,
-  onChange: () => void,
+  onChange: (value: string) => void,
   setDialogVisible: Dispatch<SetStateAction<boolean>>
 }
 
 const CitySelectContent = React.createContext<Context>({
-  map: {}, onChange: () => {}, setDialogVisible: () => {}
+  map: {}, onChange: (value: string) => {}, setDialogVisible: () => {}
 })
 
 const CitySelect: React.FC<Props> = (props) => {
@@ -23,16 +24,43 @@ const CitySelect: React.FC<Props> = (props) => {
     setDialogVisible(true)
   }
   return (
-    <CitySelectContent.Provider value={{map, onChange: () => {}, setDialogVisible}}>
+    <CitySelectContent.Provider value={{map, onChange: props.onChange, setDialogVisible}}>
       <div onClick={onClick}>{props.children}</div>
-      {dialogVisible && <Dialog></Dialog>}
+      {dialogVisible && <Dialog onClose={() => setDialogVisible(false)}></Dialog>}
     </CitySelectContent.Provider>
   )
 }
 
-const Dialog: React.FC = () => {
+const Dialog: React.FC<{ onClose: () => void } > = (props) => {
+  return ReactDOM.createPortal((
+    <div className="moore-citySelect-dialog">
+      <header>
+        <span className="icon" onClick={props.onClose}>&lt;</span>
+        <span>选择城市</span>
+      </header>
+      <CurrentLocation/>
+    </div>
+  ), document.body)
+}
+
+const CurrentLocation: React.FC = () => {
+  const [city, setCity] = useState<string>('加载中...')
+  useEffect(() => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('get', 'http://ip-api.com/json/?lang=zh-CN')
+    xhr.onload = () => {
+      const string = xhr.responseText
+      console.log(1111, string)
+    }
+    xhr.onerror = () => {
+      setCity('未知')
+    }
+    xhr.send();
+  }, [])
   return (
-    <div>1</div>
+    <div className="currentCity">
+      当前城市：{city}
+    </div>
   )
 }
 
